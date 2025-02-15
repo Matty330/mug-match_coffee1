37191b9b11c2f94246e82026fd520c0dc3d183d1
Matt <mwelc64@yahoo.com>
2025-02-15 12:44:31 -0500

    Fix: Corrected import paths and file names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,35 +2,35 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/LandingPage';
 import LandingPage from './pages/LandingPage';
-import Favorites from './pages/Favorites copy';
-import CoffeeShops from './pages/CoffeeShops copy';
+import Favorites from './pages/Favorites';  // Remove "copy"
+import CoffeeShops from './pages/CoffeeShops';  // Remove "copy"
 import Settings from './pages/ProfileSettings';
 import CoffeeQuiz from './pages/CoffeeQuiz';
 import Navbar from './components/Navbar';
-import SignUp from './pages/SignUp';
+import Signup from './pages/Signup';  // Fix casing (was SignUp)
 import Login from './pages/Login';
 import Map from './components/mapComponent';
 import Fact from './components/factComponent';
 import './index.css';
 
 function App() {
-  return  (
+  return (
     <>
       <Navbar />
-    <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/fact' element={<Fact />} />
-            <Route path="/landingpage" element={<LandingPage />} />
-            <Route path='/favorites' element={<Favorites />} />
-            <Route path='/settings' element={<Settings />} />
-            <Route path='/quiz' element={<CoffeeQuiz />} />
-            <Route path='/shops' element={<CoffeeShops />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/map' element={<Map />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fact" element={<Fact />} />
+        <Route path="/landingpage" element={<LandingPage />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/quiz" element={<CoffeeQuiz />} />
+        <Route path="/shops" element={<CoffeeShops />} />
+        <Route path="/signup" element={<Signup />} />  // Fix casing
+        <Route path="/login" element={<Login />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
     </>
   );
-};
+}
 
 export default App;
